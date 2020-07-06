bf473c86d3c727dd448362d8970e1450d44723b5
MackenzyGreen <mackenzy.green@hotmail.com>
2020-07-06 14:59:55 -0500

    navbar private route
    Made navbar dynamic to being logged in or not
    
    Made the home page a private route

--- contact-keeper/client/src/context/auth/AuthState.js
+++ contact-keeper/client/src/context/auth/AuthState.js
@@ -93,7 +93,7 @@ const AuthState = props =>{
     };
 
     // Logout
-    const logout = () => console.log('logout user');
+    const logout = () => dispatch({ type: LOGOUT });
 
 
     // Clear Errors
